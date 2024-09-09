3a59247fc066ee4be491f4c4fc4d152b6c9f4e28
Anton Androsov <aandrosov338@gmail.com>
2024-09-09 21:35:21 +0300

    Implement Home Screen

---
+++ app/src/main/kotlin/dvx/news/app/screens/HomeScreen.kt
@@ -0,0 +1,44 @@
+package dvx.news.app.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dvx.news.app.R
+import dvx.news.app.themes.DVXTheme
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(14.dp),
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        repeat(2) {
+            Image(
+                painter = painterResource(R.drawable.img_large_preview),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() = DVXTheme {
+    HomeScreen()
+}
